e4c832a63a1d205b02c2d5f5947ec5d963685fdd
sheeraz0007 <sheerazm149@gmail.com>
2024-05-08 18:29:07 +0500

    second commit

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,7 @@
-import { privateApi } from './private';
 import { publicApi } from './public';
 
 export const serviceReducers = {
-  [privateApi.reducerPath]: privateApi.reducer,
   [publicApi.reducerPath]: publicApi.reducer,
 };
 
-export const serviceMiddlewares = [privateApi.middleware, publicApi.middleware];
+export const serviceMiddlewares = [publicApi.middleware];
